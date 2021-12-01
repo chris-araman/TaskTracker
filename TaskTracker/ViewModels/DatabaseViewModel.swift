c5928004ce4f4bfd8db3b49aa477340b891572c5
Chris Araman <chris@hiddenplace.dev>
2021-12-01 14:56:59 -0800

    Handle failures

--- TaskTracker/ViewModels/DatabaseViewModel.swift
+++ TaskTracker/ViewModels/DatabaseViewModel.swift
@@ -14,51 +14,65 @@ extension View {
     }
 }
 
+@MainActor
 class DatabaseViewModel: ObservableObject {
     @Published var accountStatus = CKAccountStatus.couldNotDetermine
     @Published var tasks = [Task]()
+
+    // TODO: Display this
+    @Published var error: Error?
 
     private let database: DatabaseService
 
     init(database: DatabaseService = CloudKitDatabaseService()) {
         self.database = database
-        refresh()
+        _Concurrency.Task.detached {
+            await self.refresh()
+        }
     }
 
-    func refresh() {
-        _Concurrency.Task.detached {
-            do {
-                self.accountStatus = try await self.database.accountStatus()
-                self.tasks = try await self.database.fetchAll()
-            }
-            catch {
-                // TODO: Handle failure
-                print("Failed: \(error)")
-            }
+    func refresh() async {
+        self.error = nil
+        do {
+            self.accountStatus = try await self.database.accountStatus()
+            self.tasks = try await self.database.fetchAll()
+        }
+        catch {
+            self.error = error
         }
     }
 
     func save(_ task: Task) {
         _Concurrency.Task.detached {
-            do {
-                try await self.database.save(task.record)
-            }
-            catch {
-                // TODO: Handle failure
-                print("Failed: \(error)")
-            }
+            await self.save(task)
+        }
+    }
+
+    func save(_ task: Task) async {
+        self.error = nil
+        self.tasks.append(task)
+        do {
+            try await self.database.save(task.record)
+        }
+        catch {
+            self.error = error
         }
     }
 
     func delete(_ tasks: [Task]) {
         _Concurrency.Task.detached {
-            do {
-                try await self.database.delete(tasks.map(\.record.recordID))
-            }
-            catch {
-                // TODO: Handle failure
-                print("Failed: \(error)")
-            }
+            await self.delete(tasks)
+        }
+    }
+
+    func delete(_ tasks: [Task]) async {
+        self.error = nil
+        self.tasks.removeAll { record in tasks.contains(record) }
+        do {
+            try await self.database.delete(tasks.map(\.record.recordID))
+        }
+        catch {
+            self.error = error
         }
     }
 }
